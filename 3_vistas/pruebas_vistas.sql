b6493dc7e1dfb4f0304505ee7370693ab87e43b9
Camila Vargas <Camila.Vargas@faast.app>
2025-06-29 22:54:06 -0400

    vistas

--- 3_vistas/pruebas_vistas.sql
+++ 3_vistas/pruebas_vistas.sql
@@ -1,11 +1,29 @@
 -- 1. Usuarios con sus vehículos
 SELECT * FROM vista_usuarios_con_vehiculos;
 
--- 2. Espacios actualmente ocupados
+-- 2. Espacios ocupados actualmente
 SELECT * FROM vista_espacios_ocupados_actualmente;
 
--- 3. Espacios actualmente libres
+-- 3. Espacios libres actualmente
 SELECT * FROM vista_espacios_libres_actualmente;
 
--- 4. Reservas activas
+-- 4. Vehículos que ingresaron hoy
+SELECT * FROM vista_ingresos_hoy;
+
+
+select * from core.vehiculo where id_usuario = 121;
+select * from core.registro_parqueo where placa='ABC1211';
+-- 5. Usuarios que nunca han registrado un ingreso
+SELECT * FROM vista_usuarios_sin_ingresos;
+
+-- 6. Reservas activas actualmente
 SELECT * FROM vista_reservas_activas;
+
+-- 8. Conteo de usuarios por tipo
+SELECT * FROM vista_conteo_por_tipo_usuario;
+
+-- 9. Vehículos con estadía prolongada
+SELECT * FROM vista_vehiculos_estadia_prolongada;
+
+-- 10. Ingresos por hora diaria
+SELECT * FROM vista_ingresos_por_hora_diaria;
